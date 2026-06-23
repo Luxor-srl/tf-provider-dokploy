22224f44566d08c56166ebf6d88fb785b9706433
Patrick <patrick.guidetti2@gmail.com>
2026-06-23 17:10:05 +0200

    Fixing address

--- main.go
+++ main.go
@@ -28,10 +28,7 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
-		Address: "registry.terraform.io/ahmedali6/dokploy",
+		Address: "app.terraform.io/luxor-srl/dokploy",
 		Debug:   debug,
 	}
 
